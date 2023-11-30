395b51ba7422aca3847b0c25b71dc2eab96d191f
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-30 17:40:12 +0100

    adding modal

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
 
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
@@ -12,6 +21,7 @@ export default function App() {
       // { text: enteredGoalText, key: Math.random().toString() },
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
+    endAddGoalHandler();
   }
 
   function deleteGoalHandler(id) {
@@ -22,7 +32,17 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button
+        title="Add New Goal"
+        color="#5e0acc"
+        onPress={startAddGoalHandler}
+      />
+
+      <GoalInput
+        visible={modalIsVisible}
+        onCancel={endAddGoalHandler}
+        onAddGoal={addGoalHandler}
+      />
 
       <View style={styles.goalsConatiner}>
         {/* <ScrollView alwaysBounceVertical={false}>
